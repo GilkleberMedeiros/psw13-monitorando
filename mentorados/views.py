a6c9eb1b3ee71d3d79c12cf7915775168eac57cf
Geekki1245 <56454778+Geekki1245@users.noreply.github.com>
2025-03-31 18:15:11 -0300

    finalizando view e template mentorado.

--- mentorados/views.py
+++ mentorados/views.py
@@ -1,12 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http.response import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.messages import constants
+from django.contrib import messages
+from django.core.exceptions import ValidationError
+
+from .models import Mentorados, Navigators
 
 
 # Create your views here.
+@login_required(login_url="login")
 def mentorados(request):
+    logged_user = request.user
+
     if request.method == "GET":
-        return render(request, "mentorados.html")
+        navigators = Navigators.objects.filter(mentor=logged_user.id)
+        mentorados = Mentorados.objects.filter(mentor=logged_user)
+
+        return render(
+            request, 
+            "mentorados.html", 
+            {"estagios": Mentorados.estagio_choices, "navigators": navigators, "mentorados": mentorados}
+        )
+    
     elif request.method == "POST":
-        return HttpResponse(f"Post request para {request.path}")
+        nome = request.POST.get("nome")
+        foto = request.FILES.get("foto")
+        estagio = request.POST.get("estagio")
+        navigator = request.POST.get("navigator", None)
+
+        mentorado = Mentorados(
+            nome=nome, 
+            foto=foto, 
+            estagio=estagio, 
+            navigator_id=navigator, 
+            mentor=logged_user
+        )
+
+        try:
+            mentorado.full_clean()
+        except ValidationError as e:
+            errors = [v for v in e.message_dict.values() ]
+            messages.add_message(request, level=constants.ERRROR, message=f"{errors}")
+            return redirect("mentorados")
+
+        mentorado.save()
+
+        messages.add_message(request, level=constants.SUCCESS, message="Mentorado cadastrado com sucesso.")
+        return redirect("mentorados")
     
     return HttpResponse("Método Http não aceito.")

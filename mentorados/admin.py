38f5f341d81e70d4d39f90f2c64f77bb2f185680
Geekki1245 <56454778+Geekki1245@users.noreply.github.com>
2025-04-04 13:44:32 -0300

    registrando models Tarefa e Video na página admin.

--- mentorados/admin.py
+++ mentorados/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from .models import Mentorados, Navigators, DisponibilidadeHorario, Reuniao
+from .models import Mentorados, Navigators, DisponibilidadeHorario, Reuniao, Tarefa, Video
 
 
 admin.site.register(Mentorados)
 admin.site.register(Navigators)
 admin.site.register(DisponibilidadeHorario)
 admin.site.register(Reuniao)
+admin.site.register(Tarefa)
+admin.site.register(Video)

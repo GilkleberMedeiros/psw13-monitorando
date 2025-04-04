4a2f1842d97efea20b534f6fba6f103022c0f692
Geekki1245 <56454778+Geekki1245@users.noreply.github.com>
2025-04-04 12:55:54 -0300

    registrando models DisponobilidadeHorario e Reunaio.

--- mentorados/admin.py
+++ mentorados/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Mentorados, Navigators
+from .models import Mentorados, Navigators, DisponibilidadeHorario, Reuniao
 
 
 admin.site.register(Mentorados)
 admin.site.register(Navigators)
+admin.site.register(DisponibilidadeHorario)
+admin.site.register(Reuniao)

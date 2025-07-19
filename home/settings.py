4604987a7b04238fff7e3f23d98a1d876740b85c
Gilkleber Medeiros <gikimedei2018@gmail.com>
2025-07-19 15:08:52 -0300

    feat: adicionando requirements.txt e env vars.

--- home/settings.py
+++ home/settings.py
@@ -15,6 +15,8 @@ import os
 
 from django.contrib.messages import constants
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,12 +25,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5#=h&_#we(fa*i@=#!pdm8*j03_0_i)h0i#%qang(o=qxlbp_y'
+SECRET_KEY = config("SECRET_KEY") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", cast=bool, default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config(
+    "ALLOWED_HOSTS", 
+    cast=lambda v: [ host.strip() for host in v.split(", ")], 
+    default=[], 
+)
 
 
 # Application definition

2b093a3de97524abc933f07324351a46d2fde9c2
Gilkleber Medeiros <gikimedei2018@gmail.com>
2025-07-21 16:28:23 -0300

    fix: adicionando o pacote whitenoise para servir arquivos estáticos no host do render.

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,10 @@ COPY . .
 
 RUN python3 -m pip install -r requirements.txt
 
+RUN python3 manage.py collectstatic --no-input 
+
+RUN python3 manage.py migrate 
+
 EXPOSE 8000
 
 CMD ["gunicorn", "--bind", "0.0.0.0:8000", "home.wsgi"]
--- home/settings.py
+++ home/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware", 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,6 +132,8 @@ USE_TZ = False
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
 STATIC_ROOT = os.path.join('static')
+if not DEBUG:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
